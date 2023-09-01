fe9519b06f059a72cf2ab41686acee6ac8508ca5
Sosso Brain <Baudrymbome@gmail.com>
2023-09-01 18:01:56 -0400

    # core/urls.py update

--- core/urls.py
+++ core/urls.py
@@ -18,7 +18,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('rosetta/', include('rosetta.urls')),
-    path('', include('home.urls')),                 # include the home app urls
+    path('rosetta/',    include('rosetta.urls')),
+    path('',            include('home.urls')),                  # include the home app urls
+    path('users/',      include('users.urls')),                 # include the users app urls
 
 ]
